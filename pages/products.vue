34bc5290835a8e3bd20ef80b771ad2bad7a54fb4
AlexKY44 <oleksii.kyrychuk@oa.edu.ua>
2025-04-07 18:41:33 +0300

    feat: add products page with title

---
+++ pages/products.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Head>
+      <Title>Список продуктів</Title>
+    </Head>
+
+    <h1 class="text-2xl font-bold mb-4">Список продуктів</h1>
+
+
+    <p>Тут буде таблиця продуктів </p>
+  </div>
+</template>
+
+<script setup>
+
+</script>
